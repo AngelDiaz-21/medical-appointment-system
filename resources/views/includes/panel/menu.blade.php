debe1e25f6a64198e72b5968d53495fa1002a6c2
Angel <angelSistemas21@gmail.com>
2022-04-24 20:55:03 -0500

    fixed: Separación de menú por rol(admin, doctor y paciente)

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -1,44 +1,87 @@
-<!-- Heading -->
-<h6 class="navbar-heading text-muted">Gestionar datos</h6>
-<!-- Navigation (menu lateral izquierdo)-->
+{{-- Heading --}}
+<h6 class="navbar-heading text-muted">
+    {{-- ! Si el rol es admin verá el siguiente mensaje --}}
+    @if(auth()->user()->role == 'admin')
+    Gestionar datos
+    @else
+    Menú
+    @endif
+
+</h6>
+{{-- Navigation (menu lateral izquierdo --}}
 <ul class="navbar-nav">
+    {{-- ! Vamos hacer condiciones para ver las vistas. Si eres admin podrás ver las siguientes vistas --}}
+    @if(auth()->user()->role == 'admin')
+        <li class="nav-item">
+            <a class="nav-link" href="/home">
+                <i class="ni ni-tv-2 text-red"></i> Dashboard
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('/specialties') }}">
+                <i class="ni ni-planet text-blue"></i> Especialidades
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('/doctors') }}">
+                <i class="ni ni-single-02 text-orange"></i> Médicos
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('/patients') }}">
+                <i class="ni ni-satisfied text-info"></i> Pacientes
+            </a>
+        </li>
+    {{-- ! En cambio, si eres doctor verás la siguientes vistas --}}
+    @elseif (auth()->user()->role == 'doctor')
+        <li class="nav-item">
+            <a class="nav-link" href="/home">
+                <i class="ni ni-calendar-grid-58 text-red"></i> Gestionar horario
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('/specialties') }}">
+                <i class="ni ni-time-alarm text-primary"></i> Mis citas
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('/patients') }}">
+                <i class="ni ni-satisfied text-info"></i> Mis pacientes
+            </a>
+        </li>
+    {{-- ! Vistas para el paciente --}}
+    @else
+        <li class="nav-item">
+            <a class="nav-link" href="/home">
+                <i class="ni ni-send text-red"></i> Reservar cita
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('/specialties') }}">
+                <i class="ni ni-time-alarm text-primary"></i> Mis citas
+            </a>
+        </li>
+
+    @endif
     <li class="nav-item">
-        <a class="nav-link" href="/home">
-            <i class="ni ni-tv-2 text-red"></i> Dashboard
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ url('/specialties') }}">
-            <i class="ni ni-planet text-blue"></i> Especialidades
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ url('/doctors') }}">
-            <i class="ni ni-single-02 text-orange"></i> Médicos
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ url('/patients') }}">
-            <i class="ni ni-satisfied text-info"></i> Pacientes
-        </a>
-    </li>
-    <li class="nav-item">
-        <!-- Cuando hagamos clic se va activar un evento para que no se actualice la pagina y accedemos al elemento que tenga un ID determinado para finalmente hacer submit de dicho elemento -->
+        {{-- Cuando hagamos clic se va activar un evento para que no se actualice la pagina y accedemos al elemento que tenga un ID determinado para finalmente hacer submit de dicho elemento --}}
         <a class="nav-link" href="" onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
             <i class="ni ni-key-25"></i> Cerrar sesión
         </a>
-        <!-- Para cerrar sesión debemos de hacer una petición tipo POST -->
+        {{-- Para cerrar sesión debemos de hacer una petición tipo POST --}}
         <form action="{{ route('logout') }}" method="POST" style="display: none;" id="formLogout">
-            <!-- Como es una peticion de tipo POST tenemos que incluir un token @csrf -->
+            {{-- Como es una peticion de tipo POST tenemos que incluir un token @csrf --}}
             @csrf
         </form>
     </li>
 </ul>
-<!-- Divider -->
+{{-- ! Vamos hacer condiciones para ver las vistas. Si eres admin podrás ver las siguientes vistas --}}
+@if(auth()->user()->role == 'admin')
+{{-- Divider --}}
 <hr class="my-3">
-<!-- Heading -->
+{{-- Heading --}}
 <h6 class="navbar-heading text-muted">Reportes</h6>
-<!-- Navigation -->
+{{-- Navigation --}}
 <ul class="navbar-nav mb-md-3">
     <li class="nav-item">
         <a class="nav-link" href="#">
@@ -51,3 +94,4 @@
         </a>
     </li>
 </ul>
+@endif
